53d3f2ca75704ce5301a75b29fbec58b05522353
Abraham Burnett <abeburnett@gmail.com>
2013-07-14 00:03:05 -0600

    Up to Figure 9.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -20,7 +37,13 @@ class UsersController < ApplicationController
 
   private
     def user_params
-    params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:name, :email, :password,
       :password_confirmation)
-  end
+    end
+
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
